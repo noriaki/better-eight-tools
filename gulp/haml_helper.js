050a537c735bf5fdfffd0b79e62ad08e84d4dc47
Noriaki UCHIYAMA <uchiyama.noriaki@gmail.com>
2016-02-13 21:31:46 +0900

    fix haml_helper.

--- gulp/haml_helper.js
+++ gulp/haml_helper.js
@@ -1,23 +1,21 @@
-'use strict'
+import fs from 'fs';
+import path from 'path';
+import Haml from 'haml';
 
-import fs from 'fs'
-import path from 'path'
-import Haml from 'haml'
-
-const root = () => path.resolve('', 'src/views')
+const root = () => path.resolve('', 'src/haml');
 
 module.exports = {
   root: root(),
   render_partial: (filename, locals={}) => {
-    const extname = '.haml'
-    const basename = path.basename(filename, extname)
+    const extname = '.haml';
+    const basename = path.basename(filename, extname);
     const target = [
       path.dirname(filename), '/',
       (basename[0] === '_' ? basename : '_' + basename), extname
-    ].join('')
-    
+    ].join('');
+
     return Haml.render(
-      fs.readFileSync(path.join(root(), target), 'utf8'), {
-	pretty: true, locals: locals })
+      fs.readFileSync(path.join(root(), target), 'utf8'), { locals: locals }
+    );
   }
-}
+};
